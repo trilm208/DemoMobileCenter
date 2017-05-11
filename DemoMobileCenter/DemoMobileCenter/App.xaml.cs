2654474bd240240a625518bd0e52ba1aa90faea6
Kadence <Kadence@FW-DP02>
2017-05-11 10:30:50 +0700

    submit

--- DemoMobileCenter/DemoMobileCenter/App.xaml.cs
+++ DemoMobileCenter/DemoMobileCenter/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 using Xamarin.Forms;
 
 namespace DemoMobileCenter
@@ -19,6 +21,8 @@ namespace DemoMobileCenter
         protected override void OnStart()
         {
             // Handle when your app starts
+            MobileCenter.Start("ios=3e833b46-ea80-482c-9023-50913544ae46;android=55a9299e-3780-45d5-acb4-f25278667a54", typeof(Analytics), typeof(Crashes));
+
         }
 
         protected override void OnSleep()
